c372aff5295dd7d8f5f892615d1c88240afaa82d
Justin P <Jpartain89@gmail.com>
2017-12-16 16:19:39 -0600

    Auto Updates on "12-16-2017 16-19-39"

--- run-updates.sh
+++ run-updates.sh
@@ -7,9 +7,13 @@ FIRST_REPO="$HOME/git/ansibleParent/ansible-run-updates"
 SECOND_REPO="$HOME/git/ansibleParent/ansible-vars"
 
 for i in "${FIRST_REPO}" "${SECOND_REPO}"; do
+    echo "CDing to ${i}"
     cd "${i}" &&
+    echo "GIt Adding"
     git add . || true
+    echo "Git Commiting"
     git commit -m "Auto Updates on \"$(date '+%m-%d-%Y %H-%M-%S')\"" || true
+    echo "Git Pushing"
     git push
 done
 
@@ -18,11 +22,16 @@ ssh rpi35 '
     FOURTH_REPO="$HOME/git/ansibleParent/ansible-vars"
 
     for i in "${THIRD_REPO}" "${FOURTH_REPO}"; do
+        echo "CDing to ${i}"
         cd "${i}" &&
+        echo "GIt Adding"
         git add . || true
+        echo "Git Commiting"
         git commit -m "Auto Updates on \"$(date '+%m-%d-%Y %H-%M-%S')\"" || true
+        echo "Git Pushing"
         git push
     done
 
+    echo "Running Ansible-Playbook"
     ansible-playbook "$HOME/git/ansible-run-updates sync_config.yml" &
 '
